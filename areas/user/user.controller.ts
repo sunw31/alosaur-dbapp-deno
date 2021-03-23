641bf1c668d697c715af5cff97032d38ac8af704
borisliu <boris_cn@263.net>
2021-03-24 00:50:34 +0800

    finish coding

--- areas/user/user.controller.ts
+++ areas/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Req } from "alosaur/mod.ts";
+import { Controller, Get, Post, Put, Param, Body, QueryParam } from "alosaur/mod.ts";
 import { UserService } from "../../services/user.service.ts";
 
 @Controller("/users")
@@ -6,8 +6,26 @@ export class UserController {
   constructor(private service: UserService) {}
 
   @Get()
-  list() {
+  getAll() {
     console.log("Get all users.")
     return this.service.getAll();
+  }
+
+  @Get("/:id")
+  getById(@Param("id") id: number) {
+    console.log(`Get user with id: ${id}`);
+    return this.service.getById(id);
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    console.log(`Create a user with name: ${body.name}`);
+    return this.service.save(body.name);
+  }
+
+  @Put("/:id")
+  update(@Body() body: any, @Param("id") id: number) {
+    console.log(`Change user with id ${id} 's name to: ${body.name}`);
+    return this.service.update(id, body.name);
   }
 }
--- services/user.service.ts
+++ services/user.service.ts
@@ -1,12 +1,32 @@
 import { getCustomRepository } from "typeorm/src/index.ts";
 import { UserRepository } from "../repositories/user.repository.ts";
+import { User } from '../entities/user.entity.ts';
 
 export class UserService {
   private userRepository: UserRepository;
   constructor() {
       this.userRepository = getCustomRepository(UserRepository);
   }
+
   getAll() {
       return this.userRepository.find();
+  }
+
+  getById(id: number) {
+    return this.userRepository.findOne(id);
+  }
+
+  save(name: string) {
+    const user = new User();
+    user.name = name;
+    return this.userRepository.save(user);
+  }
+
+  async update(id: number, name: string) {
+    const user = await this.userRepository.findOne(id);
+    if (user) {
+      user.name = name;
+      return this.userRepository.save(user);
+    }
   }
 }
