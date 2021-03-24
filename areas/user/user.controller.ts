8911bc2558e944e6567ac0e861e11da39751f800
borisliu <boris_cn@263.net>
2021-03-24 10:01:20 +0800

    add error handling

--- areas/user/user.controller.ts
+++ areas/user/user.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Post,
   Put,
+  Content,
 } from "alosaur/mod.ts";
 import { UserService } from "../../services/user.service.ts";
 import { UserDto } from "../../dtos/user.dto.ts";
@@ -17,31 +18,80 @@ export class UserController {
   @Get()
   getAll() {
     console.log("Get all users.");
-    return this.service.findAll();
+    return this.service.findAll().then(users => {
+      console.log("Success to get all users");
+      return users;
+    }).catch(err => {
+      console.error("Failed to get all users");
+      console.error(err);
+      return Content({}, 400);
+    });
   }
 
   @Get("/:id")
   getById(@Param("id") id: number) {
     console.log(`Get user with id: ${id}`);
-    return this.service.findById(id);
+    return this.service.findById(id).then(user => {
+      console.log(`Success to get user with id: ${id}`);
+      return user;
+    }).catch(err => {
+      console.error(`Failed to get user with id: ${id}`);
+      console.error(err);
+      return Content({
+        "id": id
+      }, 400);
+    })
   }
 
   @Post()
   create(@Body() body: UserDto) {
     console.log(`Create a user with name: ${body.name}`);
-    return this.service.save(body.name);
+    return this.service.save(body.name).then(user => {
+      console.log(`Success to create user with name: ${body.name}`);
+      return Content({
+        "name": body.name
+      }, 201);
+    }).catch(err => {
+      console.error("Failed to create user with body:");
+      console.error(body);
+      console.error(err);
+      return Content({
+        "body": body,
+      }, 400);
+    });
   }
 
   @Put("/:id")
   update(@Body() body: UserDto, @Param("id") id: number) {
-    console.log(`Change user with id ${id} 's name to: ${body.name}`);
-    return this.service.update(id, body.name);
+    console.log(`Change the name of user with id ${id} to: ${body.name}`);
+    return this.service.update(id, body.name).then(user => {
+      console.log(`Success to change the name of user with id ${id} to: ${body.name}`);
+      return user;
+    }).catch(err => {
+      console.error(`Failed to update user with id ${id} and body`);
+      console.error(body);
+      console.error(err);
+      return Content({
+        "id": id,
+        "body": body
+      }, 400);
+    })
   }
 
   @Delete("/:id")
   delete(@Param("id") id: number) {
     console.log(`Delete user with id ${id}`);
-    this.service.delete(id);
-    return id;
+    return this.service.delete(id).then(() => {
+      console.log(`Success to delete user with id ${id}`);
+      return Content({
+        "id": id
+      }, 204);
+    }).catch(err => {
+      console.error(`Failed to delete user with id ${id}`);
+      console.error(err);
+      return Content({
+        "id": id
+      }, 400);
+    });
   }
 }
--- services/user.service.ts
+++ services/user.service.ts
@@ -1,4 +1,4 @@
-import { getCustomRepository } from "typeorm/src/index.ts";
+import { DeleteResult, getCustomRepository, UpdateResult } from "typeorm/src/index.ts";
 import { UserRepository } from "../repositories/user.repository.ts";
 import { User } from "../entities/user.entity.ts";
 
@@ -16,21 +16,17 @@ export class UserService {
     return this.userRepository.findOne(id);
   }
 
-  save(name: string) {
+  save(name: string): Promise<User> {
     const user = new User();
     user.name = name;
     return this.userRepository.save(user);
   }
 
-  async update(id: number, name: string) {
-    const user = await this.userRepository.findOne(id);
-    if (user) {
-      user.name = name;
-      return this.userRepository.save(user);
-    }
+  update(id: number, name: string): Promise<UpdateResult> {
+    return this.userRepository.update({id: id},{name: name});
   }
 
-  async delete(id: number): Promise<void> {
-    await this.userRepository.delete(id);
+  delete(id: number): Promise<DeleteResult> {
+    return this.userRepository.delete(id);
   }
 }
